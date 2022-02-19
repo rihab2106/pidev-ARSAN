41a751f2bb72f8baa4e1d5dd8c127ce132e622f0
Leonardo580 <anasbenbrahim9@gmail.com>
2022-02-19 14:31:10 +0100

    ww

--- src/com/trophy/Controller/QGamesController.java
+++ src/com/trophy/Controller/QGamesController.java
@@ -10,6 +10,7 @@ import com.trophy.Dao.TrophiesDao;
 import com.trophy.entity.Category;
 import com.trophy.entity.Games;
 import com.trophy.entity.Trophies;
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,6 +19,10 @@ import java.util.ResourceBundle;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.beans.Observable;
 import javafx.beans.property.SimpleFloatProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -30,6 +35,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -49,12 +56,18 @@ import javafx.scene.control.TitledPane;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.transform.Rotate;
 import javafx.util.Callback;
+import javafx.util.Duration;
 import javafx.util.converter.FloatStringConverter;
 import sun.plugin2.jvm.RemoteJVMLauncher;
 import sun.plugin2.jvm.RemoteJVMLauncher.CallBack;
@@ -81,7 +94,7 @@ public class QGamesController implements  Initializable {
     @FXML
     private TableColumn<Games, String> game_description;
     @FXML
-    private TableColumn<Games, ChoiceBox> game_category;
+    private TableColumn<Games, Category> game_category;
     @FXML
     private TableColumn<Games, Float> games_rate;
     @FXML
@@ -92,6 +105,7 @@ public class QGamesController implements  Initializable {
     private ContextMenu game_con;
     @FXML
     private TextField game_in_search;
+   
 
     /**
      * Initializes the controller class.
@@ -100,50 +114,57 @@ public class QGamesController implements  Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         
         
+        //Media m=new Media(new File("../../../../Resources/Mouse-Click-00-c-FesliyanStudios.com.mp3").toURI().toString());
         
-        
+       
         //tableview_game.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
         ObservableList<Games> list= GamesDao.getInstance().DisplayObservableList();
         tableview_game.setItems(list);
         game_name.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getName()));
         game_description.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getDescription()));
-        game_category.setCellValueFactory(new PropertyValueFactory<>("dummy"));
+        game_category.setCellValueFactory(new PropertyValueFactory<>("Category"));
          //game_category.setCellValueFactory(new PropertyValueFactory<>("dummy"));
         games_rate.setCellValueFactory(new PropertyValueFactory<>("Rate"));
         game_trophies.setCellValueFactory(new PropertyValueFactory<>("dummy"));
         
         
-       Callback<TableColumn<Games, String>, TableCell<Games, String>> cellFactory
-            =       (final TableColumn<Games, String> param) -> {
-    final TableCell<Games, String> cell = new TableCell<Games, String>() {
+       Callback<TableColumn<Games, Category>, TableCell<Games, Category>> cellFactory
+            =       (final TableColumn<Games, Category> param) -> {
+    final TableCell<Games, Category> cell = new TableCell<Games, Category>() {
         //final Button btn = new Button("Goto Trophies");
         ChoiceBox btn=new ChoiceBox();
         @Override
-        public void updateItem(String item, boolean empty) {
+        public void updateItem(Category item, boolean empty) {
             btn.setItems(FXCollections.observableArrayList(CategoryDao.getInstance().DisplayAllList()));
-            tableview_game.getItems().forEach(g ->{
-            btn.setValue(g.getCategory());
-            });
+            
             super.updateItem(item, empty);
+            
+            
             if (empty) {
                 setGraphic(null);
                 setText(null);
             } else {
                 btn.setOnAction(event -> {
-                    Games g=tableview_game.getSelectionModel().getSelectedItem();
-                  //  g.setCategory((Category)btn.getValue());
-                    GamesDao.getInstance().update(g);
+                    
+                  //Games g=tableview_game.getSelectionModel().getSelectedItem();
+                 // Games g=new Games();
+                  // g.setCategory((Category)btn.getValue());
+                        //System.out.println(GamesDao.getInstance().update(g));
+                    
                     
                 });
+             
+               btn.setValue(btn.getItems().get(1));
                 setGraphic(btn);
                 setText(null);
+                
             }
         }
     };
     return cell;
         };
-       game_trophies.setCellFactory(cellFactory);
-        //game_category.setCellFactory(cellFactory);
+       //game_trophies.setCellFactory(cellFactory);
+       //game_category.setCellFactory(cellFactory);
         tableview_game.setEditable(true);
         game_name.setCellFactory(TextFieldTableCell.forTableColumn());
         game_name.setOnEditCommit((CellEditEvent<Games, String> event) -> {
@@ -166,16 +187,17 @@ public class QGamesController implements  Initializable {
                 public void startEdit()
                 {
                     
-                    comboBox = new ChoiceBox();
-                    comboBox.getItems().addAll(FXCollections.observableArrayList(CategoryDao.getInstance().DisplayAllList()));
-                   comboBox.setValue(FXCollections.observableArrayList(CategoryDao.getInstance().DisplayAllList()).get(0));
-                     setGraphic(comboBox);
-                     super.updateTableView(tableview_game);
+                   
                    if(!isEmpty())
                     {
                         super.startEdit();
 
-                        
+                         comboBox = new ChoiceBox();
+                    comboBox.getItems().addAll(FXCollections.observableArrayList(CategoryDao.getInstance().DisplayAllList()));
+                   comboBox.setValue(tableview_game.getSelectionModel().getSelectedItem().getCategory());
+                     setGraphic(comboBox);
+                     super.updateTableView(tableview_game);
+                     super.updateItem(comboBox, true);
 
                         comboBox.setPrefWidth(getWidth());
                         
@@ -202,6 +224,7 @@ public class QGamesController implements  Initializable {
           GamesDao.getInstance().update(g);
           });
         
+        AudioClip mp=new AudioClip(getClass().getResource("Mouse-Click-00-c-FesliyanStudios.com.mp3").toExternalForm());
         
         add_game.setOnAction(event -> {
         
@@ -211,25 +234,29 @@ public class QGamesController implements  Initializable {
             //tableview_game.getSelectionModel().select(idx);
             Games g=new Games(CategoryDao.getInstance().DisplayAllList().get(0),"<put name>","<put Description>",5f);
             GamesDao.getInstance().insert(g);
+            mp.play();
             tableview_game.setItems(GamesDao.getInstance().DisplayObservableList());
             tableview_game.layout();
             int idx=GamesDao.getInstance().DisplayObservableList().size()-1;
+            AnimateButton(add_game);
             tableview_game.edit(idx, game_name);
         });
         
         delete_game.setOnAction(event ->{
+            mp.play();
         List<Games> l=GamesDao.getInstance().DisplayAllList();
         int ind=tableview_game.getSelectionModel().getSelectedIndex();
         GamesDao.getInstance().delete(l.get(ind));
         tableview_game.setItems(GamesDao.getInstance().DisplayObservableList());
         tableview_game.refresh();
         Alert a=new Alert(Alert.AlertType.CONFIRMATION,"Game Deleted !!",ButtonType.OK);
-        a.show();
+        a.showAndWait();
         });
         
         tableview_game.getSelectionModel().selectedItemProperty().addListener((obj, olds,newc) -> {
            
              game_acc.getPanes().clear();
+             if (tableview_game.isPressed() )
             TrophiesDao.getInstance().DisplayAllList().forEach((Trophies t) -> {
                 if (t.getGame().getId_game()==obj.getValue().getId_game()){
                     game_acc.getPanes().add(new TitledPane("Title "+t.getTitle(),DisplayTrophies(t)));
@@ -241,10 +268,13 @@ public class QGamesController implements  Initializable {
         MenuItem addtrophy=new MenuItem("Add Trophy");
         addtrophy.setOnAction(event ->{
             Games g=tableview_game.getSelectionModel().getSelectedItem();
-            Trophies t=new Trophies(g,"Nothing","Nothing","Nothing","Nothing");
-            TrophiesDao.getInstance().insert(t);
-            game_acc.getPanes().add(new TitledPane("Title :"+t.getTitle(),DisplayTrophies(t)));
-            //tableview_game.getSelectionModel().select(g);
+            Trophies tro=new Trophies(g,"Nothing","Nothing","Nothing","Nothing");
+            TrophiesDao.getInstance().insert(tro);
+            game_acc.getPanes().clear();
+            TrophiesDao.getInstance().DisplayAllList().forEach((Trophies t) -> {
+                if (t.getGame().getId_game()==g.getId_game()){
+                    game_acc.getPanes().add(new TitledPane("Title "+t.getTitle(),DisplayTrophies(t)));
+                }});
         });
         game_con.getItems().add(addtrophy);
         
@@ -275,21 +305,25 @@ public class QGamesController implements  Initializable {
         Label ltit=new Label("Title");
         Label ldiff=new Label("Difficulitiy");
         
-        TextField tplat=new TextField("nothing");
+       // TextField tplat=new TextField("nothing");
+        ChoiceBox tplat=new ChoiceBox();
+        tplat.getItems().addAll("PS5","XBox sereis X","Nintendo","PC");
         TextField ttit=new TextField("nothing");
-        TextField tdiff=new TextField("nothing");
+        //TextField tdiff=new TextField("nothing");
+        ChoiceBox tdiff=new ChoiceBox(FXCollections.observableArrayList("Very easy","Easy","Medium","Hard","Impossible"));
         TextArea tdisc=new TextArea("nothing");
         
         gp.setPadding(new Insets(10,10,10,10));
         GridPane.setMargin(gp, new Insets(20));
-        tplat.setText(t.getPlatform());
+        tplat.setValue(t.getPlatform());
         tdisc.setText(t.getDescription());
         ttit.setText(t.getTitle());
-        tdiff.setText(t.getDifficulty());
+        tdiff.setValue(t.getDifficulty());
         
-        tplat.textProperty().addListener((obj,old,ne)-> {
+        tplat.setOnAction((event)-> {
             
-        t.setPlatform(ne);
+            //System.out.println(event);
+            t.setPlatform((String)tplat.getSelectionModel().getSelectedItem());
             TrophiesDao.getInstance().update(t);
         });
         
@@ -304,9 +338,9 @@ public class QGamesController implements  Initializable {
         t.setTitle(ne);
             TrophiesDao.getInstance().update(t);
         });
-        tdiff.textProperty().addListener((obj,old,ne)-> {
+        tdiff.setOnAction(event-> {
             
-        t.setDifficulty(ne);
+        t.setDifficulty((String)tdiff.getSelectionModel().getSelectedItem());
             TrophiesDao.getInstance().update(t);
         });
         
@@ -339,5 +373,27 @@ public class QGamesController implements  Initializable {
         
         return b;
     }
+    
+    
+    public void AnimateButton(Button n){
+       n.setRotationAxis(Rotate.Z_AXIS);
+       Timeline t=new Timeline();
+        t.autoReverseProperty().set(true);
+        t.setCycleCount(4);
+       t.getKeyFrames().addAll(
+        new KeyFrame(Duration.ZERO, 
+            new KeyValue(n.translateXProperty(), 0)
+        ),
+        new KeyFrame(new Duration(1000), 
+            new KeyValue(n.translateXProperty(), 20)
+        )
+        ,new KeyFrame(Duration.millis(1000), new KeyValue(n.translateXProperty(), -50))
+    );
+       
+        t.play();
+      
+    
+    }
+    
     
 }
