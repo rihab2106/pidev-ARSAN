e0d815de7d2d0463f2fdb58fbe3ea282928f77f5
Leonardo580 <anasbenbrahim9@gmail.com>
2022-02-28 21:04:59 +0100

    my pain & sorrow

--- src/com/trophy/Controller/QGamesController.java
+++ src/com/trophy/Controller/QGamesController.java
@@ -4,25 +4,30 @@
  */
 package com.trophy.Controller;
 
+
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.deploy.net.HttpRequest;
+import com.sun.speech.freetts.VoiceManager;
+
 import com.trophy.Dao.CategoryDao;
 import com.trophy.Dao.GamesDao;
 import com.trophy.Dao.TrophiesDao;
 import com.trophy.entity.Category;
+import com.trophy.entity.ChatBot;
 import com.trophy.entity.Games;
 import com.trophy.entity.Trophies;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -79,11 +84,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
-
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
@@ -97,22 +102,38 @@ import javafx.scene.layout.StackPane;
 
 import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 import javafx.scene.transform.Rotate;
+import javafx.scene.web.WebView;
 import javafx.util.Callback;
 import javafx.util.Duration;
 import javafx.util.converter.FloatStringConverter;
 import org.controlsfx.control.NotificationPane;
 import org.controlsfx.control.Notifications;
 import org.controlsfx.control.PopOver;
+import org.controlsfx.dialog.FontSelectorDialog;
 import org.jsoup.Connection;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
+import java.util.Locale;
+import javax.speech.AudioException;
+import javax.speech.Central;
+import javax.speech.EngineException;
+import javax.speech.EngineStateError;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
+import javax.speech.synthesis.Voice;
 
 /**
  * FXML Controller class
@@ -159,6 +180,11 @@ public class QGamesController implements  Initializable {
     private MenuItem fun;
     @FXML
     private MenuItem game_sort;
+    @FXML
+    private Circle game_cercle;
+    
+   // private Synthesizer syn;
+    private com.sun.speech.freetts.Voice v;
    
 
     /**
@@ -168,10 +194,14 @@ public class QGamesController implements  Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         
         
-        //Media m=new Media(new File("../../../../Resources/Mouse-Click-00-c-FesliyanStudios.com.mp3").toURI().toString());
-        
-       
-       
+        try {
+            //Media m=new Media(new File("../../../../Resources/Mouse-Click-00-c-FesliyanStudios.com.mp3").toURI().toString());
+            
+            
+            game_cercle.setFill(new ImagePattern(new Image(new FileInputStream("png-clipart-computer-icons-desktop-chatbot-icon-blue-angle.png"))));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(QGamesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
      
          
         //tableview_game.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
@@ -354,11 +384,17 @@ public class QGamesController implements  Initializable {
              StackPane sp=new StackPane();    
             Pane root=SetUpForm(g,sp);
            
-            InputStream f=new FileInputStream("src/com/trophy/Resources/"+
+            File file=new File("src/com/trophy/Resources/"+
                         g.getName()+".jpg");
+            InputStream f;
             Image img=null;
+            if (file.exists()){
+            f=new FileInputStream(file);
+           
+            
             if (f!=null)
                img=new Image(f);
+            }
                         
                
                 ImageView i=new ImageView(img);
@@ -415,25 +451,34 @@ public class QGamesController implements  Initializable {
             }
             
         });
+        
+        /*PopOver p=new PopOver();
+        p.setTitle("Description");
+    p.setFadeOutDuration(Duration.ONE);
+    p.setFadeInDuration(Duration.ONE);
+     p.setAnimated(true);
+     p.setAutoHide(true);
         tableview_game.setRowFactory(tableView -> {
     final TableRow<Games> row = new TableRow<>();
-    PopOver p=new PopOver();
     row.hoverProperty().addListener((observable) -> {
         final Games g = row.getItem();
-
-        if (row.isHover() && g != null) {
-            Text t=new Text(g.getDescription());
+        
+        if (row.isHover() && g != null && !row.isEditing()) {
+            TextArea t=new TextArea(g.getDescription());
+            t.setEditable(false);
+            t.setWrapText(true);
             p.setContentNode(t);
-            p.setAnimated(true);
+           
+            p.setWidth(600);
             
             p.show(row);
         } else {
-            
+            p.hide(Duration.ONE);
         }
     });
 
     return row;
-});
+});*/
         
         
         
@@ -450,8 +495,63 @@ public class QGamesController implements  Initializable {
             }
 
         });
+        MenuItem toFrench=new MenuItem("To french");
         
-        game_con.getItems().addAll(fetchInfo,lookFurther);
+         toFrench.setOnAction(e -> {
+            try {
+                Games g =g=tableview_game.getSelectionModel().getSelectedItem();
+                Document d=Jsoup.connect("https://api.nlpcloud.io/v1/opus-mt-en-fr/translation")
+                        .header("Authorization", "Token 27e8cb58e8a53c25f1bf61e5d32d8fa1b9b6d8a0")
+                        .ignoreContentType(true)
+                        .userAgent("Mozilla")
+                        //.data("'text'","'John Doe has been working for Microsoft in Seattle since 1999.'")
+                        .ignoreHttpErrors(true)
+                        .requestBody("{\"text\":\""+g.getDescription().replaceAll("\n", "")
+                                .replaceAll("\r", "")+"\"}")
+                        .post();
+                ObjectMapper obj=new ObjectMapper();
+                JsonNode jn=obj.readTree(d.body().text());
+                g.setDescription(jn.get("translation_text").asText());
+                GamesDao.getInstance().update(g);
+                tableview_game.setItems(GamesDao.getInstance().DisplayObservableList());
+                
+            } catch (IOException ex) {
+                Logger.getLogger(QGamesController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         
+        
+
+         });
+        
+        MenuItem fontSelector=new MenuItem("Fonts");
+        fontSelector.setOnAction(ee -> {
+            FontSelectorDialog s=new FontSelectorDialog(null);
+            s.showAndWait();
+            tableview_game.getParent().setStyle(""
+                    + ".root{"
+                    + "-fx-font-size: "+s.getResult().getSize()+"pt;" +
+                    "    -fx-font-family: \""+s.getResult().getFamily()+"\";}");
+            
+        });
+        game_con.getItems().addAll(fetchInfo,lookFurther,toFrench,fontSelector);
+        /*youtube =YouTube.Builder(new NetHttpTransport(),new Jackson(),e -> {})
+                .setApplicationName("youtube-cmdline-search-sample").build();
+        YouTube.Search.List search = youtube.search().list("id,snippet");*/
+        System.setProperty("freetts.voices",
+                "com.sun.speech.freetts.en.us"
+                    + ".cmu_us_kal.KevinVoiceDirectory");
+        
+            /*Central.registerEngineCentral("com.sun.speech.freetts"
+                    + ".jsapi.FreeTTSEngineCentral");
+             syn=Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
+            syn.allocate();
+            syn.resume();*/
+            v=VoiceManager.getInstance().getVoice("kevin16");
+            v.allocate();
+        
+  
+        
+      
         
               
     }    
@@ -527,9 +627,41 @@ public class QGamesController implements  Initializable {
         TrophiesDao.getInstance().delete(t);
         game_acc.getPanes().remove(game_acc.getExpandedPane());
         });
+        Button vtuto=new Button("watch the Tutoial");
+        vtuto.setOnAction(ev -> {
+            String q=t.getGame().getName()+" "+t.getTitle()+" walkthrough";
+            PopOver pop=new PopOver();
+            pop.setTitle("Tutorial video");
+            
+            try {
+                q=URLEncoder.encode(q,"UTF-8");
+                Response d=Jsoup.connect("https://www.googleapis.com/youtube/v3/search?q="+q+"&key=AIzaSyAv2wjYsP23B90uykgf3jpX8Dtpwsmze3U")
+                               .ignoreContentType(true)
+                               .execute();
+                ObjectMapper obj=new ObjectMapper();
+                JsonNode jn=obj.readTree(d.body());
+                String vidId=jn.get("items").get(1).get("id").get("videoId").asText();
+                //Media m=new Media("https://www.youtube.com/watch?v=CzVOzkgX65M");
+                WebView w=new WebView();
+                w.getEngine().load("https://www.youtube.com/embed/"+vidId);
+                //MediaPlayer mp=new MediaPlayer(m);
+                //mp.play();
+                //MediaView mv=new MediaView(mp);
+                pop.setContentNode(w);
+                pop.setDetached(true);
+                pop.setArrowLocation(PopOver.ArrowLocation.LEFT_TOP);
+                pop.show(vtuto);
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(QGamesController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(QGamesController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         
-        
-        b.getChildren().addAll(gp,delete);
+        });
+        b.setSpacing(10);
+        HBox hbox=new HBox(delete,vtuto);
+        hbox.setSpacing(20);
+        b.getChildren().addAll(gp,hbox);
         b.setAlignment(Pos.CENTER);
         
         return b;
@@ -744,20 +876,32 @@ public class QGamesController implements  Initializable {
                         .substring(len-4, len));
      img.write(img_res.bodyAsBytes());
      img.close();
+     String genre=jn.get("genres").get(0).get("name").asText();
+     if (CategoryDao.getInstance().DisplayAllList()
+             .stream().noneMatch(e -> e.getCategory().equals(genre)))
+         CategoryDao.getInstance().insert(new Category(genre));
+     Category c=CategoryDao.getInstance().DisplayAllList().stream()
+             .filter(e ->e.getCategory().equals(genre))
+             .findFirst()
+             .orElse(null);
+     g.setCategory(c);
+                    
         
     return g;
     
     }
     
     public String FunFact() throws IOException{
-    Element d =Jsoup.connect("https://asli-fun-fact-api.herokuapp.com/")
-            .ignoreContentType(true).get().body();
-    String json =d.text();
+    Response d =Jsoup.connect("https://api.api-ninjas.com/v1/facts")
+            .header("X-Api-Key", "F5coi8YxdewL4rDKo0FVVw==mM5geLpb1Ki9o5P0")
+            .ignoreContentType(true).execute();
+    String json =d.body();
     ObjectMapper obj=new ObjectMapper();
        
-    JsonNode jn=obj.readTree(obj.getFactory().createParser(json));
+    JsonNode jn=obj.readTree(json);
         
-    return jn.get("data").get("fact").asText();
+    return jn.get(0).get("fact").asText();
+    
     
     
     }
@@ -782,6 +926,60 @@ public class QGamesController implements  Initializable {
     @FXML
     private void SortGames(ActionEvent event) {
         tableview_game.setItems(GamesDao.getInstance().Sort());
+    }
+
+    @FXML
+    private void showFromWeb(MouseEvent event) {
+            PopOver pop=new PopOver();
+            TextField tx=new TextField();
+            pop.setTitle("Leonardo");
+            pop.setAnimated(true);
+            pop.setAutoFix(true);
+            pop.setArrowLocation(PopOver.ArrowLocation.RIGHT_TOP);
+            VBox box=new VBox(tx);
+            box.setStyle("-fx-background-color: #373e43");
+            tx.setOnAction(e -> {
+            Label rb=new Label();
+            
+                try {
+                    System.out.println(tx.getText());
+                    String cat=ChatBot.BotRespond(tx.getText());
+                    String res=ChatBot.questionAnswer.get(cat);
+                   if (cat.contains("add-game")){
+                       Category c=CategoryDao.getInstance().displayById(1);
+                   GamesDao.getInstance().insert(new Games(c,"<put name>","<put name>",0.0f));
+                   tableview_game.setItems(GamesDao.getInstance().DisplayObservableList());
+                   }
+                   else if (cat.contains("modify-game")){
+                   
+                   }
+                   else if (cat.contains("delete-game")){
+                   String name=tx.getText();
+                   name=name.substring(name.lastIndexOf(" "));
+                    
+                   
+                   Games g=(Games)((GamesDao.getInstance().searchByName(name)
+                           .isEmpty()) ? new Games():GamesDao.getInstance().searchByName(name)
+                           .get(0));
+                   GamesDao.getInstance().delete(g);
+                   res+=" "+g.getName();
+                   }
+                   else if (cat.contains("show-stat")){
+                       show_stat(e);
+                   }
+                    rb.setText("Bot : "+res);
+                   v.speak(res);
+                    tx.clear();
+                } catch (IOException ex) {
+                    Logger.getLogger(QGamesController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalArgumentException ex) {
+                    Logger.getLogger(QGamesController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                box.getChildren().addAll(new Label("You : "+tx.getText()),rb);
+            });
+            
+            pop.setContentNode(box);
+            pop.show(game_cercle);
     }
     
     
